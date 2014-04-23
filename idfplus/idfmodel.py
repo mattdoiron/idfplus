d59de15a5632a96067c74ac16d09d2bf9abba865
Matt Doiron <mattdoiron@gmail.com>
2014-04-22 23:43:40 -0700

    cleaned up the idfmodel file and simplified data model to use more basic pyton data types

--- idfplus/idfmodel.py
+++ idfplus/idfmodel.py
@@ -20,29 +20,13 @@ along with Foobar. If not, see <http://www.gnu.org/licenses/>.
 from collections import OrderedDict
 
 
-class MissingIDDFile(Exception):
-    pass
-
-
-class MissingFilePath(Exception):
-    pass
-
-
-class InvalidIDFClass(Exception):
-    pass
-
-
-class InvalidIDFObject(Exception):
-    pass
-
-
-class InvalidField(Exception):
-    pass
-
-
 class IDDFile(OrderedDict):
-    '''Primary object representing idd file and container for idd objects.'''
+    '''
+    Primary object representing idd file and container for idd objects.
+    Contains an OrderedDict of IDDObjects with the class type as key.
+    '''
 
+    # Various properties of the idd file
     version = None      # EnergyPlus vesion number (eg. 8.1.0.800)
     groups = []         # Groups to which various classes can belong
     data_path = 'data/EnergyPlus_IDD_v{}.dat'
@@ -63,8 +47,12 @@ class IDDFile(OrderedDict):
 
 
 class IDFFile(OrderedDict):
-    '''Primary object representing idf file and container for idf objects.'''
+    '''
+    Primary object representing idf file and container for idf objects.
+    Contains an OrderedDict of IDFObjects with the class type as key.
+    '''
 
+    # Various properties of the idf file
     version = None      # EnergyPlus vesion number (eg. 8.1.0.800)
     options = []        # options that may have been found in idf file
     file_path = None    # full, absolute path to idf file
@@ -73,76 +61,13 @@ class IDFFile(OrderedDict):
 
     def __init__(self, file_path, *args, **kwargs):
         super(IDFFile, self).__init__(*args, **kwargs)
-
-        if not file_path:
-            raise MissingFilePath
-        else:
-            self.file_path = file_path
-
         self.__load(self)
 
-#    def options(self):
-#        '''Retrieves current options for the idf file.'''
-#        return self.options
-
-#    def setOptions(self, opts):
-#        '''Sets options for the idf file'''
-#        if opts in self.options:
-#            return True
-#        if not opts in self.options:
-#            self.options.append(opts)
-#            return True
-#        else:
-#            return False
-
-#    def addObject(self, cls, index, obj):
-#        '''adds a new idf object to the specified idf file.
-#        Validates entries and required fields against the idd file.'''
-#        if cls in self:
-#            try:
-#                self[cls].insert(index, obj)
-#                return True
-#            except IndexError:
-#                raise InvalidIDFObject
-#        else:
-#            raise InvalidIDFClass
-
-#    def removeObject(self, cls, index):
-#        '''deletes the specified idf object'''
-#        if cls in self:
-#            try:
-#                self[cls].pop(index)
-#                return True
-#            except IndexError:
-#                raise InvalidIDFObject
-#        else:
-#            raise InvalidIDFClass
-
-#    def idfObject(self, cls, index, contains=None):
-#        '''retrieves and idf object from the given idf file'''
-#        if cls in self:
-#            try:
-#                return self[cls][index]
-#            except IndexError:
-#                raise InvalidIDFObject
-#        else:
-#            raise InvalidIDFClass
-
-#    def idfClass(self, cls):
-#        '''retrieves all idf objects of a certain class from the
-#        given idf file'''
-#        if cls in self:
-#            return self[cls]
-#        else:
-#            raise InvalidIDFClass
-
-    def find(self, contains=None):
-        '''Searches within the file for objects having "contains"'''
-        pass
-
     def __load(self):
-        '''parses and loads an idf file into the object (instance
-        variable stores it)'''
+        '''
+        Parses and loads an idf file into the object instance variable.
+        Also sets some properties of the file.
+        '''
 
         import idfparse
 
@@ -155,95 +80,70 @@ class IDFFile(OrderedDict):
         self._OrderedDict__update(objects)
         self.idd = idd
 
-        # parse idf file then
-        # set class types to be empty IDFClass objects then
-        # set groups, version, eol_char, etc
+    def find(self, contains=None):
+        '''Searches within the file for objects having "contains"'''
         pass
 
     def save(self, file_path):
-        '''Handles writing of the idf file to disk.'''
+        '''Handles writing of the idf file back to disk.'''
         pass
 
 
-#class IDFClass(list):
-#    '''Container for idf objects'''  # is this needed?
-#
-#    group = None
-#    class_type = None
-#
-#    def __init__(self, group, class_type, *args, **kwargs):
-#        super(IDFClass, self).__init__(*args, **kwargs)
-#        self.group = group
-#        self.class_type = class_type
-
-
-class IDDObject(OrderedDict):
-    '''Represents objects in idd files.'''
-
-    obj_tags = None
-    comments_special = None
-    group = None
-    obj_class = None
-    tags = None  # expand to show all possible tags as properties?
-
-    def __init__(self, *args, **kwargs):
-        # Use kwargs to prepopulate some values, then remove them from kwargs
-        self.obj_tags = kwargs.pop('obj_tags', None)
-        self.comments_special = kwargs.pop('comments_special', None)
-        self.group = kwargs.pop('group', None)
-        self.obj_class = kwargs.pop('obj_class', None)
-        super(IDDObject, self).__init__(*args, **kwargs)
-
-
 class IDFObject(OrderedDict):
-    '''Represents objects in idf files.'''
+    '''
+    Represents objects in idf files.
 
+    Contains an OrderedDIct of fields in the form:
+        {'A1': 'field1 value',
+         'N1': 123.23,
+         'A2': 'field value 213'}
+    '''
+
+    # Various properties of the idf object
     idd = None
     comments = None         # User comments for field
     obj_class = None        # Class type of object
-#    incomming_links = []
-#    outgoing_links = []
+    group = None            # Group to which this object belongs
+    incomming_links = {}    # List of objects that link to this one
+    outgoing_links = {}     # List of objects to which this one links
 
     def __init__(self, idd, *args, **kwargs):
-        # Use kwargs to prepopulate some values, then remove them from kwargs
-        self.obj_tags = kwargs.pop('obj_tags', None)
-        self.field_tags = kwargs.pop('field_tags', None)
+        '''
+        Use kwargs to prepopulate some values, then remove them from kwargs
+        Also sets the idd file for use by this object.
+        '''
+        self.idd = idd
         self.comments = kwargs.pop('comments', None)
-        self.comments_special = kwargs.pop('comments_special', None)
-        self.group = kwargs.pop('group', None)
         self.obj_class = kwargs.pop('obj_class', None)
+        self.group = kwargs.pop('group', None)
         super(IDFObject, self).__init__(*args, **kwargs)
 
     def field(self, fld):
-        '''Returns a field object of type IDFField'''
+        '''Returns a field's value by id or index'''
         if type(fld) is int:
             return self[self.keys()[fld]]
         elif type(fld) is str:
             self.get(fld)
 
-    @property
-    def tags(self):
-        '''Returns list of tags like "default", "required", etc, from idd.'''
-        return self.idd[self.obj_class].tags
+    def add_link(self, obj, field_id, incomming=False, outgoing=False):
+        '''
+        Ads a link to and/or from this object.
+        obj should be another IDFObject and field_id like 'A1' or 'N2'
+        '''
+        if not incomming and not outgoing:
+            return False
+        if incomming:
+            self.incomming_links.update({field_id: obj})
+        if outgoing:
+            self.outgoing_links.update({field_id: obj})
+        return True
 
-    @property
-    def comments_pecial(self):
-        '''Returns the comments from idd file'''
-        pass
-
-    @property
-    def group(self):
-        '''Returns the group to which this obj belongs (from idd file)'''
-        pass
-
-    @property
     def incomming_links(self):
         '''Returns list of notes that reference any fields in this object.
         (How will it know that a field is a node? not sure it's possible.
         Check idd file)'''
         pass
 
-    @property
     def outgoing_links(self):
         '''Returns list of nodes referenced by any fields in this object.
         (How will it know that a field is a node? not sure it's possible.
@@ -251,35 +151,22 @@ class IDFObject(OrderedDict):
         pass
 
 
-class IDFField(object):
-    '''Basic component of the idf object classes.'''
+class IDDObject(OrderedDict):
+    '''Represents objects in idd files.
 
-    idd = None
-    obj_class = None
-    value = None
-    field_id = None
+    Simply a regular OrderedDict containing IDDField objects with the keys
+    being the class type.
+    '''
 
-    def __init__(self, idd, obj_class, field_id, value, *args, **kwargs):
-        super(IDFField, self).__init__(*args, **kwargs)
-        self.idd = idd
-        self.obj_class = obj_class
-        self.field_id = field_id
-        self.value = value
+    pass
 
-    def __repr__(self):
-        return self.value
 
-    @property
-    def fieldType(self):
-        '''Returns field type (eg: alphanumeric, number, choice, etc) from idd'''
-        pass
+class IDDField(object):
+    '''Basic component of the idd object classes.
 
-    @property
-    def tags(self):
-        '''Returns list of tags like "default", "required", etc, from idd.'''
-        return self.idd[self.obj_class][self.field_id].tags
+    Simply a regular dict containing keys in the form of 'A1' or 'N2' and
+    values for each of the idd field properties in the following list:
+        required, field, type, minimum, etc.
+    '''
 
-    @property
-    def comments(self):
-        '''Returns comments for field from idd.'''
-        pass
+    pass
