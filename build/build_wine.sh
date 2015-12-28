05a20eb6cdb84102df918ff632adc06d6fe58bff
Matt Doiron <mattdoiron@gmail.com>
2015-12-27 21:32:07 -0800

    Significant work on build_wine.sh. Slowly adding WIX capabilities.

--- build/build_wine.sh
+++ build/build_wine.sh
@@ -1,71 +1,80 @@
 #!/usr/bin/env bash
 
-# This script provisions a wind prefix and uses it to build the windows version of IDFPlus
+# This script provisions a WINE prefix and uses it to build the windows version of IDFPlus
 
-export BUILDDIR=$PWD
-export DISTDIR=$PWD/../dist
-export WINEPREFIX=${BUILDDIR}/idfplus_prefix
-export WINEARCH=win32
-export WINEDEBUG=-all,err+all
+prepare_env () {
+    export BUILD_DIR=$PWD
+    export DIST_DIR=$PWD/../dist
+    export WINEPREFIX=${BUILD_DIR}/idfplus_prefix
+    export WINEARCH=win32
+    export WINEDEBUG=-all,err+all
+    export WIX_DIR=${BUILD_DIR}/wix310
+}
 
 download_prerequisites () {
     # Check for some files and download them if necessary
     echo "Checking for required install files..."
-    if [ ! -f ${BUILDDIR}/python-2.7.11.msi ]
+    if [ ! -f ${BUILD_DIR}/python-2.7.11.msi ]
     then
       echo "Downloading Python 2.7.11..."
-      wget --directory-prefix=${BUILDDIR} --show-progress --quiet \
+      wget --directory-prefix=${BUILD_DIR} --show-progress --quiet \
         https://www.python.org/ftp/python/2.7.11/python-2.7.11.msi
     fi
-    if [ ! -f ${BUILDDIR}/VCForPython27.msi ]
+    if [ ! -f ${BUILD_DIR}/VCForPython27.msi ]
     then
       echo "Downloading Visual C++ for Python..."
-      wget --directory-prefix=${BUILDDIR} --show-progress --quiet \
+      wget --directory-prefix=${BUILD_DIR} --show-progress --quiet \
         https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi
     fi
-    if [ ! -f ${BUILDDIR}/vcredist_x86.exe ]
+    if [ ! -f ${BUILD_DIR}/vcredist_x86.exe ]
     then
       echo "Downloading Visual C++ Redistributable 2008..."
-      wget --directory-prefix=${BUILDDIR} --show-progress --quiet \
+      wget --directory-prefix=${BUILD_DIR} --show-progress --quiet \
         https://download.microsoft.com/download/1/1/1/1116b75a-9ec3-481a-a3c8-1777b5381140/vcredist_x86.exe
     fi
-    if [ ! -f ${BUILDDIR}/mono-4.2.1.102-gtksharp-2.12.30-win32-0.msi ]
+    if [ ! -f ${BUILD_DIR}/mono-4.2.1.102-gtksharp-2.12.30-win32-0.msi ]
     then
       echo "Downloading Mono..."
-      wget --directory-prefix=${BUILDDIR} --show-progress --quiet \
+      wget --directory-prefix=${BUILD_DIR} --show-progress --quiet \
         "http://download.mono-project.com/archive/4.2.1/windows-installer/mono-4.2.1.102-gtksharp-2.12.30-win32-0.msi"
     fi
-    if [ ! -f ${BUILDDIR}/wix310-binaries.zip ]
+    if [ ! -f ${BUILD_DIR}/wix310-binaries.zip ]
     then
       echo "Downloading WIX Toolset..."
-      wget --directory-prefix=${BUILDDIR} --show-progress --quiet \
+      wget --directory-prefix=${BUILD_DIR} --show-progress --quiet \
         "http://wixtoolset.org/downloads/v3.10.1.2213/wix310-binaries.zip"
+    fi
+    if [ ! -f ${BUILD_DIR}/PSDK-x86.exe ]
+    then
+      echo "Downloading Windows Server 2003 SP1 Platform SDK..."
+      wget --directory-prefix=${BUILD_DIR} --show-progress --quiet \
+        "https://download.microsoft.com/download/a/5/f/a5f0d781-e201-4ab6-8c6a-9bb4efed1e1a/PSDK-x86.exe"
     fi
 }
 
 install_prerequisites () {
     echo "Installing prerequisites..."
     winetricks win7
-    wine ${BUILDDIR}/vcredist_x86.exe /q
-    wine msiexec /i ${BUILDDIR}/VCForPython27.msi /qn ALLUSERS=1
-    wine msiexec /i ${BUILDDIR}/python-2.7.11.msi /qn ALLUSERS=1 TARGETDIR=C:\Python27
+    wine ${BUILD_DIR}/vcredist_x86.exe /q
+    wine msiexec /i ${BUILD_DIR}/VCForPython27.msi /qn ALLUSERS=1
+    wine msiexec /i ${BUILD_DIR}/python-2.7.11.msi /qn ALLUSERS=1 TARGETDIR=C:\Python27
     wine msiexec /i ${BUILDDIR}/mono-4.2.1.102-gtksharp-2.12.30-win32-0.msi /qn ALLUSERS=1
+    wine ${BUILD_DIR}/PSDK-x86.exe
 #    wineboot -ru
-#    winetricks --unattended dotnet40
 
     echo "Extracting WIX..."
-    unzip -q -n ${BUILDDIR}/wix310-binaries.zip -d wix310
+    unzip -q -n ${BUILD_DIR}/wix310-binaries.zip -d wix310
 
     echo "Installing python dependencies..."
     wine python -m pip install persistent -q
     wine python -m pip install pyinstaller -q
-    wine python -m pip install -r ../requirements.txt -q
+#    wine python -m pip install -r ../requirements.txt -q
 }
 
 build () {
     echo "Building IDFPlus for windows..."
     wine python -m PyInstaller --clean --noconfirm --onedir --windowed \
-        --distpath=${DISTDIR} --workpath=${BUILDDIR} \
+        --distpath=${DIST_DIR} --workpath=${BUILD_DIR} \
         --icon=../resources/images/logo.ico --upx-dir=../resources/upx/upx391w \
         --additional-hooks-dir=../resources \
         --win-no-prefer-redirects --win-private-assemblies \
@@ -73,31 +82,58 @@ build () {
 }
 
 make_installer () {
-    # Harvest initial files, but not needed afterwards
-    #wine ${BUILDDIR}/wix310/heat.exe dir ${DISTDIR}/idfplus/ -gg -sfrag -srd -template product -sw5150 -out idfplus_harvest.wxs
+    export WINE_DISTDIR=$(winepath -w ${DIST_DIR})
 
     echo "Running candle..."
-    export WINE_DISTDIR=$(winepath -w $DISTDIR)
-    wine ${BUILDDIR}/wix310/candle.exe -nologo -out ${WINE_DISTDIR}/idfplus.wixobj idfplus.wxs
+    wine ${WIX_DIR}/candle -nologo -out ${WINE_DISTDIR}/idfplus.wixobj idfplus.wxs
 
     echo "Running light..."
-    wine ${BUILDDIR}/wix310/light.exe -nologo -sh -sval -spdb -b ${WINE_DISTDIR}/idfplus/ -out ${WINE_DISTDIR}/idfplus.msi ${WINE_DISTDIR}/idfplus.wixobj
-#    wine ${BUILDDIR}/wix310/light.exe
+    wine ${WIX_DIR}/light -nologo -sh -sval -spdb -b ${WINE_DISTDIR}/idfplus/ \
+        -out ${WINE_DISTDIR}/idfplus.msi ${WINE_DISTDIR}/idfplus.wixobj
 }
 
-# Check arguments
-if [ "$1" == 'build' ]; then
-    build
-elif [ "$1" == 'provision' ]; then
-    download_prerequisites
-    install_prerequisites
-elif [ "$1" == 'installer' ]; then
-    make_installer
-elif [ "$1" == 'all' ]; then
-    download_prerequisites
-    install_prerequisites
-    build
-    make_installer
-else
-    echo "Please specify either 'provision', 'build', 'installer' or 'all'."
-fi
+harvest () {
+    export WINE_DISTDIR=$(winepath -w ${DIST_DIR})
+
+    echo "Harvesting file info..."
+    wine ${WIX_DIR}/heat dir ${DIST_DIR}/idfplus/ -nologo -g1 -gg \
+        -cg IDFPlusComponents -sfrag -srd -template product -sw5150 -sw5151 \
+        -out ${WINE_DISTDIR}/idfplus_harvest.wxs
+}
+
+clean () {
+    echo "Cleaning prefix..."
+    rm -r ${WINEPREFIX}
+}
+
+# Prepare necessary environment variables
+prepare_env
+
+# Detect command line arguments
+case "$1" in
+    build)
+        build
+        ;;
+    provision)
+        download_prerequisites
+        install_prerequisites
+        ;;
+    installer)
+        make_installer
+        ;;
+    all)
+        download_prerequisites
+        install_prerequisites
+        build
+        make_installer
+        ;;
+    clean)
+        clean
+        ;;
+    harvest)
+        harvest
+        ;;
+    *)
+        echo $"Usage: $0 {build|provision|installer|all|clean|harvest}"
+        exit 1
+esac
