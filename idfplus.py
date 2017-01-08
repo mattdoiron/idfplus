7b5f3fb477a4fe7732b6a475e3c60169166a849f
Matt Doiron <mattdoiron@gmail.com>
2017-01-08 15:27:14 -0800

    Remove __main__.py and more startup code back to idfplus.py No reason to have it in __main__.py

--- idfplus.py
+++ idfplus.py
@@ -20,6 +20,46 @@ along with IDF+. If not, see <http://www.gnu.org/licenses/>.
 # Prepare for Python 3
 from __future__ import (print_function, division, absolute_import)
 
-from idfplus import __main__
+# Standard library imports
+import sys
+import os
+import faulthandler
+import argparse
+
+# PySide imports
+from PySide import QtGui
+
+# Local imports
+from idfplus import config
+from idfplus.main import IDFPlus
+
+
+def process_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', nargs='?', default=None)
+    return parser.parse_known_args()
+
+
+def main():
+    """Main function to start the program.
+    """
+
+    with open(os.path.join(config.LOG_DIR, 'idfplus_error_dump.txt'), 'w') as f:
+        faulthandler.enable(file=f)
+
+        # Parse command-line arguments
+        parsed_args, unparsed_args = process_args()
+        qt_args = sys.argv[:1] + unparsed_args
+
+        # Retrieve the currently running instance or make a new one
+        app = QtGui.QApplication.instance()
+        if app is None:
+            app = QtGui.QApplication(qt_args)
+
+        # Create the main window and show it
+        idf_plus_window = IDFPlus(args=parsed_args)
+        idf_plus_window.show()
+        sys.exit(app.exec_())
+
 if __name__ == '__main__':
-    __main__.main()
+    main()
