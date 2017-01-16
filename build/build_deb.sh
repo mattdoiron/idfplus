fb53f594b3dea5b3456ed359dfeec0419856678e
Matt Doiron <mattdoiron@gmail.com>
2017-01-15 22:15:06 -0800

    Tweaks to script that builds .deb files.

--- build/build_deb.sh
+++ build/build_deb.sh
@@ -16,13 +16,14 @@
 # along with IDF+. If not, see <http://www.gnu.org/licenses/>.
 #
 
-# This script builds the Ubuntu version of IDFPlus
+# This script builds the Debian/Ubuntu version of IDFPlus
 
 # The OS used to build the .deb file will be the LOWEST version supported. So, use
 # an older (preferably LTS) version if possible.
 
 # The following prerequisites are required:
-# sudo apt-get install build-essential debhelper lintian
+# sudo apt-get install build-essential debhelper lintian ruby ruby-dev rubygems gcc make
+# sudo gem install --no-ri --no-rdoc fpm
 
 # Define the necessary environment variables
 prepare_env () {
@@ -36,13 +37,13 @@ make_installer () {
     chmod -R 0755 ${DIST_DIR}/idfplus/
 
     fpm --name idfplus --version ${VERSION} --license GPL3 --architecture i386 \
-    --description "IDF+ is an enhanced editor for EnergyPlus input files\n
+    --description "IDF+ is an enhanced editor for EnergyPlus input files.
     This editor continues the tradition of a lightweight interface to IDF files." \
     --iteration 1 --category Editor --url https://bitbucket.org/mattdoiron/idfplus \
-    --prefix /usr/share -C ${DIST_DIR} --no-depends --no-deb-use-file-permissions \
+    --prefix /usr/share --chdir ${DIST_DIR} --no-depends --no-deb-use-file-permissions \
     --maintainer "Matt Doiron <mattdoiron@gmail.com>" \
     --deb-changelog ${DIST_DIR}/../CHANGELOG.rst \
-    --force --package ${DIST_DIR} -s dir -t deb idfplus
+    --force --package ${DIST_DIR} --input-type dir --output-type deb idfplus
 }
 
 prepare_env
