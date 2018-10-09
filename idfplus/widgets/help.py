192712eb9c0c8291cc6cb312c2419b327c1e28d1
Matt Doiron <mattdoiron@gmail.com>
2018-10-08 17:04:43 -0700

    Remove references to QtWebKit for now. Currently unused and is chellenging to import.

--- idfplus/widgets/help.py
+++ idfplus/widgets/help.py
@@ -6,37 +6,37 @@
 :license: GPL v3, see LICENSE for more details.
 """
 
-# System imports
-import logging
-
-# PySide imports
-from PySide import QtWebKit
-from PySide import QtGui
-from PySide import QtCore
-
-# Setup logging
-log = logging.getLogger(__name__)
-
-
-class WebViewer(QtWebKit.QWebView):
-
-    def __init__(self, parent=None):
-        super(WebViewer, self).__init__(parent)
-        self.settings = QtWebKit.QWebSettings.globalSettings()
-        # self.settings.setAttribute(QtWebKit.QWebSettings.LocalContentCanAccessFileUrls, True)
-        # self.settings.setAttribute(QtWebKit.QWebSettings.LocalContentCanAccessRemoteUrls, True)
-        self.settings.setAttribute(QtWebKit.QWebSettings.DeveloperExtrasEnabled, True)
-
-
-class HelpWindow(QtGui.QMainWindow):
-
-    def __init__(self, *args, **kwargs):
-        super(HelpWindow, self).__init__(*args, **kwargs)
-        self.web_view = WebViewer(self)
-        self.web_view.load(QtCore.QUrl('http://bigladdersoftware.com/epx/docs/8-6/index.html'))
-        self.setWindowTitle(self.web_view.title())
-        layout = QtGui.QHBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(self.web_view)
-        self.setLayout(layout)
-        self.setCentralWidget(self.web_view)
+# # System imports
+# import logging
+#
+# # PySide imports
+# from PySide import QtWebKit
+# from PySide import QtGui
+# from PySide import QtCore
+#
+# # Setup logging
+# log = logging.getLogger(__name__)
+#
+#
+# class WebViewer(QtWebKit.QWebView):
+#
+#     def __init__(self, parent=None):
+#         super(WebViewer, self).__init__(parent)
+#         self.settings = QtWebKit.QWebSettings.globalSettings()
+#         # self.settings.setAttribute(QtWebKit.QWebSettings.LocalContentCanAccessFileUrls, True)
+#         # self.settings.setAttribute(QtWebKit.QWebSettings.LocalContentCanAccessRemoteUrls, True)
+#         self.settings.setAttribute(QtWebKit.QWebSettings.DeveloperExtrasEnabled, True)
+#
+#
+# class HelpWindow(QtGui.QMainWindow):
+#
+#     def __init__(self, *args, **kwargs):
+#         super(HelpWindow, self).__init__(*args, **kwargs)
+#         self.web_view = WebViewer(self)
+#         self.web_view.load(QtCore.QUrl('http://bigladdersoftware.com/epx/docs/8-6/index.html'))
+#         self.setWindowTitle(self.web_view.title())
+#         layout = QtGui.QHBoxLayout()
+#         layout.setContentsMargins(0, 0, 0, 0)
+#         layout.addWidget(self.web_view)
+#         self.setLayout(layout)
+#         self.setCentralWidget(self.web_view)
